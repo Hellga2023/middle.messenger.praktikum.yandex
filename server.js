b45384de2a9d686c1dcbedddbf198e1f7f4cf804
Olga Kuptsova <helllga2018@yandex.ru>
2023-03-13 16:39:19 +0400

    adding all files

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static(__dirname+"/dist"));
+
+app.listen(PORT, function () { console.log(`My app listening on port ${PORT}!`);});  
+
+app.get('/*', function(req, res) {
+  res.sendFile(__dirname + '/dist/index.html');
+});
---
+++ src/components/auth_input/auth_input.tmpl.js
@@ -0,0 +1,8 @@
+const auth_input = `
+<div class="input-container">
+    <label class="input-container__label">{{label}}</label>   
+    <input class="input-container__input" type="{{type}}" name="{{name}}"/> 
+</div>`;
+
+export default auth_input;
+
