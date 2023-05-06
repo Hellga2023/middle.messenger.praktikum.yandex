6ad697de5a99b12da15261f3de31e43a8be6a055
Olga Kuptsova <helllga2018@yandex.ru>
2023-05-06 21:36:34 +0400

    Http class tests added

---
+++ src/utils/http.test.ts
@@ -0,0 +1,42 @@
+import { expect } from "chai";
+import { HTTP, METHODS } from "./http";
+import sinon from 'sinon';
+
+describe('HTTP class',()=>{
+
+    const XHR = sinon.useFakeXMLHttpRequest();
+    const requests:sinon.SinonFakeXMLHttpRequest[] = [];
+
+    //@ts-ignore
+    global.XMLHttpRequest = XHR;
+
+    XHR.onCreate = function (xhr) {
+        requests.push(xhr);
+    };
+
+    afterEach(()=>{
+        requests.length = 0;
+    });
+
+    it('should call xhr with GET method if get called',()=>{
+        const http = new HTTP("/auth");
+        http.get("/",{});
+        expect(requests[0].method).to.eq(METHODS.GET);
+    });
+
+    it('should call xhr with POST method if post called',()=>{
+        const http = new HTTP("/auth");
+        http.post("/login",{a:'a'});
+        expect(requests[0].method).to.eq(METHODS.POST);
+    });
+
+    /*
+    check if params were passed right
+    check if url is auth/login
+    
+    it('should call xhr with POST method if post called',()=>{
+        const http = new HTTP("/auth");
+        http.post("/login",{a:'a'});
+        expect(requests[0].method).to.eq(METHODS.POST);
+    });*/
+});
